67537b04bfac7d974c2a50b483881a44456f4adf
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2019-10-16 15:09:11 +0300

    test 16.10

---
+++ task2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char s[100];
+    scanf("%s", &s);
+    int i = 0;
+    int n = 0;
+    while(s[i]){
+        n = n * 10;
+        n = n + (s[i] - '0');
+        i++;
+    }
+    printf("%d", n);
+    return 0;
+}
---
+++ task3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int s;
+    int res[256] = {0};
+    char n[256] = {0};
+    for (int i = 0; i < N; i++){
+        scanf("%d", &s);
+        n[s] += 1;
+        if (n[s] == 1){
+            res[i] = s;
+        }
+    }
+    for (int j = 0; j < N; j++){
+        if(res[j] != 0){
+            printf("%d ", res[j]);
+        }
+    }
+    return 0;
+}
