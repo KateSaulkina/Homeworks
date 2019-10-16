e31c84461e6bff8bc47661f58b1066d491e41250
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2019-10-16 15:16:59 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -6,7 +6,7 @@ int main()
     int N;
     scanf("%d", &N);
     int s;
-    int res[256] = {0};
+    int res[10000];
     char n[256] = {0};
     for (int i = 0; i < N; i++){
         scanf("%d", &s);
