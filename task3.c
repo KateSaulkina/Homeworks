f7a5b4982569577329129f264566bf505a0dd92c
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2019-10-23 02:17:07 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -6,19 +6,26 @@ int main()
     int N;
     scanf("%d", &N);
     int s;
-    int res[10000];
-    char n[256] = {0};
-    for (int i = 0; i < N; i++){
+    int order = 1;
+    int num1[256] = {0};
+    int reit[256] = {0};
+    for (int i = 0; i < N; i++) {
         scanf("%d", &s);
-        n[s] += 1;
-        if (n[s] == 1){
-            res[i] = s;
+        reit[s] += 1;
+        if (reit[s] == 1) {
+            num1[s] = order;
+            ++order;
         }
     }
-    for (int j = 0; j < N; j++){
-        if(res[j] != 0){
-            printf("%d ", res[j]);
+    for (int i = 1; i <= 256; i++) {
+        for (int j = 0; j < 256; j++) {
+            if (num1[j] == i) {
+                printf("%d ", j);
+            }
         }
     }
+
+
+
     return 0;
 }
