600aa6e39ea4088b0f9ba23e361b6616dfdfbf33
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2019-11-19 17:44:46 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -4,14 +4,20 @@
 int fib(int n) {
     if (n == 0 || n == 1) {
         return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
     }
-    return fib(n - 1) + fib(n - 2);
 }
 
 int main()
 {
     int n;
+    printf("Enter the number: ");
     scanf("%d", &n);
-    printf("%d", fib(n));
+    if (n < 0) {
+        printf("Error: negative number");
+    } else {
+        printf("Fibonacci number: %d", fib(n));
+    }
     return 0;
 }
