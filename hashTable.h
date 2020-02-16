34910e0731e3cbc43e45924e99f90f0cca6252cc
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2020-02-17 00:47:35 +0300

    Update hashTable.h

--- hashTable.h
+++ hashTable.h
@@ -9,8 +9,8 @@ struct HashTable {
 };
 
 struct HashTable *createHashTable(size_t (*hashFunc) (char* word), size_t k);
-void addEl(struct HashTable *table, char *word, size_t hash);
-void deleteEl(struct HashTable *table, char *word);
+void addWord(struct HashTable *table, char *word, size_t hash);
+void deleteWord(struct HashTable *table, char *word);
 size_t getData(struct HashTable *table, char *word);
 void setData(struct HashTable*, char *word, size_t val);
 void deleteTable(struct HashTable *table);
