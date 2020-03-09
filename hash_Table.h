628cc0107307b06ab10cf50cee32cca692a57d11
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2020-03-09 15:24:56 +0300

    Create hash_Table.h

---
+++ hash_Table.h
@@ -0,0 +1,19 @@
+#ifndef HASHTABLE_H_INCLUDED
+#define HASHTABLE_H_INCLUDED
+#include "listHash.h"
+
+struct HashTable {
+	struct List** entry;
+	size_t size;
+	size_t (*hashFunc) (char* word);
+};
+
+struct HashTable *createHashTable(size_t (*hashFunc) (char* word), size_t k);
+void addWord(struct HashTable *table, char *word, int val, size_t hash);
+void deleteWord(struct HashTable *table, char *word);
+size_t getData(struct HashTable *table, char *word);
+void setData(struct HashTable*, char *word, size_t val);
+void deleteTable(struct HashTable *table);
+void statistics(struct HashTable *table);
+
+#endif // HASHTABLE_H_INCLUDED
