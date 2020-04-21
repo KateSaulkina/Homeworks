9eb8155666b6ea07e28a7ddefcfa5db9f529bd8c
KateSaulkina <54909330+KateSaulkina@users.noreply.github.com>
2020-04-21 03:08:00 +0300

    Update hash_Table.h

--- hash_Table.h
+++ hash_Table.h
@@ -1,19 +1,20 @@
-#ifndef HASHTABLE_H_INCLUDED
-#define HASHTABLE_H_INCLUDED
+#ifndef HASH_TABLE_H_INCLUDED
+#define HASH_TABLE_H_INCLUDED
 #include "listHash.h"
 
 struct HashTable {
 	struct List** entry;
 	size_t size;
-	size_t (*hashFunc) (char* word);
+	size_t(*hashFunc) (char* word);
 };
 
-struct HashTable *createHashTable(size_t (*hashFunc) (char* word), size_t k);
-void addWord(struct HashTable *table, char *word, int val, size_t hash);
-void deleteWord(struct HashTable *table, char *word);
-size_t getData(struct HashTable *table, char *word);
-void setData(struct HashTable*, char *word, size_t val);
-void deleteTable(struct HashTable *table);
-void statistics(struct HashTable *table);
+struct HashTable* createHashTable(size_t(*hashFunc) (char* word), size_t k);
+void addWord(struct HashTable* table, char* word, int val, size_t hash);
+void deleteWord(struct HashTable* table, char* word);
+size_t getData(struct HashTable* table, char* word);
+void setData(struct HashTable*, char* word, size_t val);
+void deleteTable(struct HashTable* table);
+void statistics(struct HashTable* table);
+int findWord(struct HashTable* table, char* word);
 
-#endif // HASHTABLE_H_INCLUDED
+#endif // HASH_TABLE_H_INCLUDED
